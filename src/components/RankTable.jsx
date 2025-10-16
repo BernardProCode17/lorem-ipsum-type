096d8d6850eed6b463654a854461ca7096a5af72
Bernard Clarke <bernardanthonyclarke1292@gmail.com>
2025-10-15 20:28:49 -0700

    started the styling of the rank table, remove divs around table data

--- src/components/RankTable.jsx
+++ src/components/RankTable.jsx
@@ -4,32 +4,37 @@ export default function RankTable() {
         <section className="lt-rank-main">
             <h2 className="rank-main-title">Rankings</h2>
             <div>
+
+                {/* table head */}
                 <table className="rank-table">
-                    <tr className="rank-table-row">
-                        <th className="rank-table-head">Rank</th>
-                        <th className="rank-table-head">Name</th>
-                        <th className="rank-table-head">Score</th>
+                    {/* table head */}
+                    <tr className="rank-table-row-head">
+                        <th className="rank-table-head head-rank">Rank</th>
+                        <th className="rank-table-head head-name">Name</th>
+                        <th className="rank-table-head head-score">Score</th>
                     </tr>
+                
+                {/* table rows */}
+                <tr className="rank-table-row">                 
+                        <td className="rank-table-data data-rank">1</td>
+                        <td className="rank-table-data data-name">Bernard Clarke</td>
+                        <td className="rank-table-data data-score">550p</td>                   
+                </tr>
 
-                    <tr className="rank-table-row">
-                        <div>
-                            <td className="rank-table-data">1</td>
-                            <td className="rank-table-data">First Rank</td>
-                            <td className="rank-table-data">550p</td>
-                        </div>
-                        <div>
-                            <td className="rank-table-data">2</td>
-                            <td className="rank-table-data">Second Rank</td>
-                            <td className="rank-table-data">530p</td>
-                        </div>
-                        <div>
-                            <td className="rank-table-data">3</td>
-                            <td className="rank-table-data">Third Rank</td>
-                            <td className="rank-table-data">500p</td>
-                        </div>
+                <tr className="rank-table-row">
+                        <td className="rank-table-data data-rank">2</td>
+                        <td className="rank-table-data data-name">Second Rank</td>
+                        <td className="rank-table-data data-score">530p</td>
+                </tr>
 
-                    </tr>
+                <tr className="rank-table-row">
+                        <td className="rank-table-data data-rank">3</td>
+                        <td className="rank-table-data data-name">Third Rank</td>
+                        <td className="rank-table-data data-score">500p</td>
+                </tr>
+
                 </table>
+
             </div>
         </section>
     )

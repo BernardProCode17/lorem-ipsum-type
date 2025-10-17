0fa8696192a818deb62016a50154b68db76c7997
Bernard Clarke <bernardanthonyclarke1292@gmail.com>
2025-10-17 11:22:54 -0700

    added and styled user score modal, refactored gameuser file for stying

--- src/components/GameUser.jsx
+++ src/components/GameUser.jsx
@@ -1,29 +1,34 @@
-export default function GameUser(){
+export default function GameUser() {
 
-    return(
-        <div>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" />
+    return (
+        <section className="lt-rank-modal">
+
+            <h2 className="rank-modal-title">Enter Rank</h2>
+
+            <div className="rank-modal-input modal-username">
+                <label htmlFor="username" className="modal-input-label">Username</label>
+                <input type="text" className="modal-input-username" placeholder="Name For Rank" />
             </div>
 
-            <div>
-                <label htmlFor="score">Score</label>
-                <span>550p</span>
+            <div className="rank-modal-input modal-score">
+                <label htmlFor="score" className="modal-input-label">Score</label>
+                <span className="modal-input-score">0</span>
             </div>
 
-            <div>
-                <label htmlFor="abb">Abbrivation Code</label>
-                <input type="text" name="abb" id="abbcode" />
+            <div className="rank-modal-input modal-abb">
+                <label htmlFor="abb" className="modal-input-label">Abbrivation Code</label>
+                <input type="text" name="abb" id="abbcode" className="modal-input-abb" maxLength={5} placeholder="T9e5" />
             </div>
 
-            <div>
-                <label htmlFor="pin">Pin</label>
-                <input type="number" name="pin" id="pincode" />
+            <div className="rank-modal-input modal-pin">
+                <label htmlFor="pin" className="modal-input-label">Pin</label>
+                <input type="text" name="pin" id="pincode" minLength={4} maxLength={5} className="modal-input-pin" placeholder="9733" />
             </div>
 
-            <button>Rank</button>
+            <div className="rank-modal-input">
+                <button className="modal-button">Rank</button>
+            </div>
 
-        </div>
+        </section>
     )
 }
